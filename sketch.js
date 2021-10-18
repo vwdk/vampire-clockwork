bf55c11ae7b4223248e620aaa2df471a23c555cc
vwdk <92760429+vwdk@users.noreply.github.com>
2021-10-18 17:46:24 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+var ball
+function setup() {
+    createCanvas(500,700);
+ball = createSprite(250,250,10,10)
+  }
+
+
+function draw() 
+{
+  background(30);
+drawSprites()
+if(keyDown("down")) {
+
+ball.y +=1
+
+
+
+
+}
+
+
+
+
+
+
+
+}
+
+
+
+
